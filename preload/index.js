797b7390be539486f63492cde30331e9aaa71e38
wunaiquan <wunaiquan@126.com>
2021-11-01 10:57:47 +0800

    优化

--- preload/index.js
+++ preload/index.js
@@ -5,14 +5,12 @@ const getDigitConvertList = (numStr) => {
   if (isQian) numStr = numStr.replace(/,/g, '')
   const list = [{
     title: Nzh.cn.toMoney(numStr, { outSymbol: false }),
-    description: '复制 - 人民币大写',
-    icon: 'yuan.png'
+    description: '复制 - 大写人民币'
   }]
   if (!isQian && numStr.split('.')[0].length > 3) {
     list.push({
       title: numStr.replace(/\d+/, s => s.replace(/(\d)(?=(\d{3})+$)/g, '$1,')),
-      description: '复制 - 人民币千分位',
-      icon: 'yuan.png'
+      description: '复制 - 人民币千分位'
     })
   }
   return list
@@ -24,8 +22,9 @@ window.exports = {
     args: {
       placeholder: '输入阿拉伯数字',
       search: (action, searchWord, callbackSetList) => {
+        searchWord = searchWord.trim()
         if (!searchWord) return callbackSetList([])
-        if (!/\d{1,15}(?:\.\d{1,2})?|\d{1,3}(?:,\d{3}){1,4}/.test(searchWord)) {
+        if (!/^(\d{1,15}|\d{1,3}(?:,\d{3}){1,4})(?:\.\d{1,2})?$/.test(searchWord)) {
           return callbackSetList([])
         }
         callbackSetList(getDigitConvertList(searchWord))
@@ -42,17 +41,9 @@ window.exports = {
     args: {
       enter: (action) => {
         window.utools.hideMainWindow()
-        if (/^\d/.test(action.payload)) {
-          const result = Nzh.cn.toMoney(action.payload.replace(/,/g, ''), { outSymbol: false })
-          window.utools.copyText(result)
-          window.utools.showNotification('"' + result + '" 已复制')
-          window.utools.outPlugin()
-          return
-        }
-        const num = Nzh.cn.decodeB(action.payload)
-        if (typeof num !== 'number') return
-        window.utools.copyText(num.toString())
-        window.utools.showNotification('"' + num + '" 已复制')
+        const result = Nzh.cn.toMoney(action.payload.replace(/,/g, ''), { outSymbol: false })
+        window.utools.copyText(result)
+        window.utools.showNotification('"' + result + '" 已复制')
         window.utools.outPlugin()
       }
     }
